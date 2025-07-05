fd250a175e196cb8d7e2009013dc35b9d2d0a33d
parthasarkar878 <parthasarkar878@gmail.com>
2025-07-05 22:45:45 +0530

    removed deprecated mongoose options

--- server.js
+++ server.js
@@ -10,10 +10,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.log(err));
 
